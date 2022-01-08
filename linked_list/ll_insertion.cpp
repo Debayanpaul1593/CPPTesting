22ac9321320acfac7a1770c237501f282e9d9c8e
Debayanpaul1593 <debayan.sec@gmail.com>
2022-01-08 21:31:24 +0530

    fixes: some changes

--- linked_list/ll_insertion.cpp
+++ linked_list/ll_insertion.cpp
@@ -5,65 +5,69 @@ public:
     int data;
     Node *next;
 };
-
-void push(Node **head_ref, int _data)
+class LinkedList
 {
-    Node *new_node = new Node();
-    new_node->data = _data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-
-void print(Node *temp)
-{
-    while (temp != NULL)
+public:
+    Node *head = NULL;
+    Node** head_ref = &head;
+    void push(int _data)
     {
-        std::cout << temp->data << "\t";
-        temp = temp->next;
+        Node *new_node = new Node();
+        new_node->data = _data;
+        new_node->next = (*head_ref);
+        (*head_ref) = new_node;
     }
-}
 
-void append(Node **head_ref, int new_data)
-{
-    Node *last = *head_ref;
-    Node *new_node = new Node();
-    new_node->data = new_data;
-    new_node->next = NULL;
-    if (*head_ref == NULL)
+    void print()
     {
-        *head_ref = new_node;
+        Node* temp = head; 
+        while (temp != NULL)
+        {
+            std::cout << temp->data << "\t";
+            temp = temp->next;
+        }
+    }
+
+    void append(int new_data)
+    {
+        Node *last = *head_ref;
+        Node *new_node = new Node();
+        new_node->data = new_data;
+        new_node->next = NULL;
+        if (*head_ref == NULL)
+        {
+            *head_ref = new_node;
+            return;
+        }
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = new_node;
         return;
     }
-    while (last->next != NULL)
-    {
-        last = last->next;
-    }
-    last->next = new_node;
-    return;
-}
 
-void insertAfter(Node *prev_node, int new_data)
-{
-    if (prev_node == NULL)
+    void insertAfter(Node *prev_node, int new_data)
     {
-        std::cout << "Previous node cannot be NULL" << std::endl;
-        return;
+        if (prev_node == NULL)
+            std::cout << "Previous node cannot be NULL" << std::endl;
+        {
+            return;
+        }
+        Node *new_node = new Node();
+        new_node->data = new_data;
+        new_node->next = prev_node->next;
+        prev_node->next = new_node;
     }
-    Node *new_node = new Node();
-    new_node->data = new_data;
-    new_node->next = prev_node->next;
-    prev_node->next = new_node;
-}
+};
 
 int main()
 {
-    Node *head_ref = NULL;
-    push(&head_ref, 5);
-    push(&head_ref, 7);
-    push(&head_ref, 8);
-    append(&head_ref, 2);
-    append(&head_ref, 6);
-    insertAfter(head_ref->next, 12);
-    print(head_ref);
+    LinkedList ll;
+    ll.append(1);
+    ll.append(3);
+    ll.append(5);
+    ll.append(7);
+    ll.print();
     return 0;
 }
